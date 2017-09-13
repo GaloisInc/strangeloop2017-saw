2500dfa522cea8046f0780411d26b6a51f1e1b67
Aaron Tomb <atomb@galois.com>
2017-09-13 14:11:35 -0700

    Return fuzzer example to return codes

--- examples/fuzzer.c
+++ examples/fuzzer.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 
 int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
-  volatile int *ptr = NULL;
   int bits = 0;
   if (Size > 0 && Data[0] == 'F') bits |= 1;
   if (Size > 1 && Data[1] == 'U') bits |= 2;
@@ -12,7 +11,7 @@ int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
   if (Size > 4 && Data[4] == 'E') bits |= 16;
   if (Size > 5 && Data[5] == 'R') bits |= 32;
   if (bits == 63) {
-      return *ptr;
+    assert(0);
   }
   return 0;
 }
